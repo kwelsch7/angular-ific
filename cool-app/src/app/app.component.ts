671a427d9e01f0e6e958747208ea64d50d43c821
Konnor Welsch <kwelsch@2ndwatch.com>
2018-01-27 14:16:35 -0800

    Create project

---
+++ cool-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+const randomVariable = 2 + 3;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'super duper apper scooper';
+  numNum = randomVariable;
+}
+
+
